2ce7b9a7579663d0d42fcb976b7f2826690d9304
Maurice Barnett <mauricb@g.clemson.edu>
2021-01-05 18:44:36 -0500

    Vaguely added schedule, need to clean it up better... grab all teams, then cross reference with ID to fix name? Maybe only save the IDs from the URL. Need to add logic to add year to the date as well

--- poc.py
+++ poc.py
@@ -6,8 +6,8 @@ import pprint
 BASE_URL    = "https://www.espn.com/mens-college-basketball/team/{schedule,roster}/_/id/{team_id}/season/{year}"
 GAME_URL    = "https://www.espn.com/mens-college-basketball/game?gameId=401258866"
 PBP_URL     = "https://www.espn.com/mens-college-basketball/playbyplay?gameId=401265031"
-SCH_URL     = "https://www.espn.com/mens-college-basketball/team/schedule/_/id/228/season/2021"
-RST_URL     = "https://www.espn.com/mens-college-basketball/team/roster/_/id/228/season/2021"
+SCH_URL     = "https://www.espn.com/mens-college-basketball/team/schedule/_/id/228/season/2020"
+RST_URL     = "https://www.espn.com/mens-college-basketball/team/roster/_/id/228/season/2020"
 TEAMS_URL   = "https://www.espn.com/mens-college-basketball/teams"
 
 VERBOSE = True
@@ -42,10 +42,30 @@ def get_teams():
 def get_schedule():
     r = requests.get(SCH_URL)
     soup = BeautifulSoup(r.content, 'html5lib')
-    print('To be determined')
+    raw_games = soup.find_all('div', attrs={'class': 'flex items-center opponent-logo'})
+    schedule = []
+
+    for raw_game in raw_games:
+        sections = raw_game.parent.parent.find_all('td', attrs={'class': 'Table__TD'})
+        raw_game_span = raw_game.find_all('span')
+        opp_team_url = raw_game_span[-1].find('a')['href'].split('/')
+        schedule.append({
+            'location': 'Home' if raw_game_span[0].get_text() == 'vs' else 'Away',
+            'opponent': {
+                'name': raw_game_span[-1].get_text().strip(),
+                'id': opp_team_url[opp_team_url.index('id') + 1]
+            },
+            'date': sections[0].get_text()
+        })
+    
+    if VERBOSE:
+        pprint.pprint(schedule)
+
+    return schedule
 
 ###         ###
 # Main Runner #
 ###         ###
 if __name__ == '__main__':
-    #get_teams()
+    # get_teams()
+    get_schedule()
